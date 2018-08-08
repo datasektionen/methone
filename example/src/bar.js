a26d42c83b408c81ef96ac0dbb83233b82a4da04
Andreas Mårtensson <addem1234@gmail.com>
2018-08-08 21:51:02 +0200

    eslint fix

--- example/src/bar.js
+++ example/src/bar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Methone from 'methone';
+import Methone from 'methone'
 
 window.methone_conf = {
   color_scheme: 'cerise',
@@ -14,9 +14,8 @@ window.methone_conf = {
       ...config
     }
 
-    ReactDOM.render(<Methone config={window.methone_conf} />, document.getElementById('methone-container-replace'));
+    ReactDOM.render(<Methone config={window.methone_conf} />, document.getElementById('methone-container-replace'))
   }
 }
 
-
-window.methone_conf.update();
+window.methone_conf.update()
