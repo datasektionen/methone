eea6a8f19de14342efa4a80d5b4edb2845475c93
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-02-06 19:12:37 +0100

    Rewrite example page, include header
    Rewrites the example page to a much nicer one. Also included the header
    and updated the code skeleton example.

--- example/src/index.js
+++ example/src/index.js
@@ -2,44 +2,96 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Link } from 'react-router-dom'
 
-import Methone from 'methone'
+import Methone, { Header } from 'methone'
 
 // This is an example that can be used during development
 
-window.methone_conf = {
-  color_scheme: 'cerise',
-  system_name: 'Example',
-  links: [
-    <Link to="/info" key="info">Info</Link>,
-    {
-      str: 'Test text',
-      href: '/more/test'
-    },
-    {
-      str: 'Other text that is long',
-      href: '/more/text'
-    },
-    {
-      str: 'Another one',
-      href: '/another/one'
-    },
-  ],
-  login_text: 'Login',
-  login_href: '/login',
-  ...window.methone_conf,
-  update: config => {
-    window.methone_conf = {
-      ...window.methone_conf,
-      ...config
-    }
-
-    const app = <BrowserRouter>
-      <Methone config={window.methone_conf} />
-    </BrowserRouter>
-
-    ReactDOM.render(app, document.getElementById('methone-container-replace'))
+const Example = () => {
+  
+  const methone_conf = {
+    color_scheme: 'cerise',
+    system_name: 'Example',
+    links: [
+      <Link to="/info" key="info">Info</Link>,
+      {
+        str: 'Test text',
+        href: '/more/test'
+      },
+      {
+        str: 'Other text that is long',
+        href: '/more/text'
+      },
+      {
+        str: 'Another one',
+        href: '/another/one'
+      },
+    ],
+    login_text: true ? "Login" : "Log out",
+    login_href: true ? "/login" : "/logout",
   }
+
+  return (
+    <BrowserRouter>
+      <div id="application" className="cerise">
+        <Methone config={methone_conf} />
+        <Header title="Methone" action={{onClick: _ => alert("Hej!"), text: "Test"}}>
+          <Link to="#">« Tillbaka</Link>
+        </Header>
+        <div style={{width: "100%", justifyContent: "center", alignItems: "center", display: "flex"}}>
+          <CodeExample />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
+const CodeExample = () => {
+  return (
+    <pre style={{width: "70%"}}>{`
+# methone
 
-window.methone_conf.update()
+> The worlds first Top-Bar-as-a-Service
+
+## Install
+
+
+npm install --save git+https://github.com/datasektionen/Methone.git
+
+## Usage (React example, functional component)
+----------------------------------------------------------------------
+import React, { Component } from 'react'
+import { BrowserRouter, Link } from 'react-router-dom'
+
+import Methone, { Header } from 'methone'
+
+class Example = () => {
+  const config = {
+    color_scheme: 'cerise',
+    system_name: 'Example',
+    links: [
+      <Link to="/info">Info</Link>
+    ],
+    login_text: 'Login',
+    login_href: '/login',
+  }
+
+  return (
+    <BrowserRouter>
+      <div id="application" className="cerise">
+        <Methone config={config} />
+        <Header title="Methone" action={{onClick: _ => alert("Hej!"), text: "Test"}}> // Action optional, don't pass props to not render
+          <Link to="#">« Tillbaka</Link> // Optional
+        </Header>
+        <div>
+          // ...body here
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+----------------------------------------------------------------------
+    `}</pre>
+  )
+}
+
+ReactDOM.render(<Example />, document.getElementById('methone-container-replace'))
