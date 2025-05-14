073143e742f9a82f1aaaf02dbc5a5c174abc79e7
Fredrik Blomqvist <104637174+frblo@users.noreply.github.com>
2025-05-14 12:45:46 +0200

    Verklighetsförankring (#34)
    Fixes #29
    
    * remove old shit
    
    * add new shit
    
    * Update src/fuzzyfile.js
    
    Co-authored-by: Mathias Magnusson <mathias@magnusson.space>
    
    ---------
    
    Co-authored-by: Mathias Magnusson <mathias@magnusson.space>

--- src/fuzzyfile.js
+++ src/fuzzyfile.js
@@ -5,7 +5,12 @@ export default {
       "name": "Organisation - Datasektionen.se",
       "str": "organisation",
       "color": "#e83d84",
-      "href": "https://datasektionen.se/organisation"
+      "href": "https://datasektionen.se/sektionen/organisation"
+    },{
+      "name": "Aaallt - Resten",
+      "str": "aaallt aallt allt resten system alla",
+      "color": "#ffa726",
+      "href": "https://aaallt.datasektionen.se"
     },{
       "name": "Näringsliv - Datasektionen.se",
       "str": "näringsliv industry contact",
@@ -22,15 +27,20 @@ export default {
       "color": "#e91e63",
       "href": "https://calypso.datasektionen.se"
     },{
-      "name": "Zfinger",
-      "str": "xfinger zfinger",
-      "color": "#00bcd4",
-      "href": "https://zfinger.datasektionen.se"
+      "name": "Cashflow - Kvittosystemet",
+      "str": "kvitto kvitton budget",
+      "color": "#1b6c25",
+      "href": "https://cashflow.datasektionen.se"
     },{
       "name": "Skywhale - Valsystemet",
       "str": "val valsystem skywhale",
       "color": "#06b",
       "href": "https://val.datasektionen.se"
+    },{
+      "name": "Styrdokument",
+      "str": "styrdokument stadgar reglemente",
+      "color": "#e93b85",
+      "href": "https://styrdokument.datasektionen.se"
     },{
       "name": "Pandora - Bokningssystemet",
       "str": "bandora bokning bil kamera lokalbokning",
@@ -57,15 +67,20 @@ export default {
       "color": "#0ab87a",
       "href": "https://dbu.gg"
     },{
-      "name": "Motioner",
-      "str": "motioner",
-      "color": "white",
-      "href": "https://motioner.datasektionen.se"
-    },{
       "name": "Metaspexet",
       "str": "metaspexet",
       "color": "red",
       "href": "http://metaspexet.se"
+    },{
+      "name": "Tiki - Biljettsystem",
+      "str": "biljetter tiki biljettsystem",
+      "color": "#000000",
+      "href": "https://tiki.datasektionen.se/"
+    },{
+      "name": "Mattermost",
+      "str": "mattermost metamost",
+      "color": "#1e325c",
+      "href": "https://mattermost.datasektionen.se"
     },{
       "name": "Damm",
       "str": "damm historia",
